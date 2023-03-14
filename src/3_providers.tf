1a0418ad902756a8387e845ecdeb3db3de6664d1
Lazizbek Kahramonov <lazizbekkahramonov@gmail.com>
2023-03-14 18:04:34 +0500

    application and build&push job added to actions

--- src/3_providers.tf
+++ src/3_providers.tf
@@ -15,7 +15,7 @@ provider "aws" {
   # access_key = var.aws_access_key
   # secret_key = var.aws_secret_key
 
-  default_tags {
-    tags = var.tags
-  }
+  # default_tags {
+  #   tags = var.tags
+  # }
 }
