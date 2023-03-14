4a0addce1df0408cdd270ee9db5df24abe529b0f
Lazizbek Kahramonov <lazizbekkahramonov@gmail.com>
2023-03-14 19:18:52 +0500

    Added the Prometheus-client, exposed http count  endpoint

--- application/app/index.ts
+++ application/app/index.ts
@@ -1,7 +1,20 @@
 import * as express from 'express';
+import * as promClient from 'prom-client';
 import 'express-async-errors';
 
 const app = express();
+
+// Define a Prometheus counter metric
+const counter = new promClient.Counter({
+  name: 'myapp_request_count',
+  help: 'Number of requests to my app',
+});
+
+// Increase the counter metric for each incoming request
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  counter.inc();
+  next();
+});
 
 app.get('*', (req: express.Request, res: express.Response) => {
   const response = {
@@ -11,6 +24,12 @@ app.get('*', (req: express.Request, res: express.Response) => {
   };
 
   res.status(200).send(response);
+});
+
+// Expose the Prometheus metrics on a /metrics endpoint
+app.get('/metrics', (req: express.Request, res: express.Response) => {
+  res.set('Content-Type', promClient.register.contentType);
+  res.send(promClient.register.metrics());
 });
 
 app.listen(3000, () => {
