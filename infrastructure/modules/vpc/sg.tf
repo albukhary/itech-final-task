90c649e45230a71522c214e09a340593e57f7ffe
Lazizbek Kahramonov <lazizbekkahramonov@gmail.com>
2023-03-19 16:09:42 +0500

    added loose sg rules

--- infrastructure/modules/vpc/sg.tf
+++ infrastructure/modules/vpc/sg.tf
@@ -5,11 +5,12 @@ resource "aws_security_group" "eks_cluster" {
   vpc_id      = aws_vpc.this.id
 
   tags = {
-    Name = "${var.project}-cluster-sg"
+    Name                                           = "${var.project}-cluster-sg"
+    "kubernetes.io/cluster/${var.project}-cluster" = "owned"
   }
 }
 
-resource "aws_security_group_rule" "cluster_inbound" {
+resource "aws_security_group_rule" "cluster_inbound_nodes" {
   description              = "Allow worker nodes to communicate with the cluster API Server"
   from_port                = 0 #443
   protocol                 = "tcp"
@@ -19,17 +20,7 @@ resource "aws_security_group_rule" "cluster_inbound" {
   type                     = "ingress"
 }
 
-resource "aws_security_group_rule" "cluster_alb_webhook_inbound" {
-  description       = "Open communication to AWS Load Balancer webhook service"
-  from_port         = 9443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.eks_cluster.id
-  cidr_blocks       = ["0.0.0.0/0"]
-  to_port           = 9443 #443
-  type              = "ingress"
-}
-
-resource "aws_security_group_rule" "cluster_outbound" {
+resource "aws_security_group_rule" "cluster_outbound_nodes" {
   description              = "Allow cluster API Server to communicate with the worker nodes"
   from_port                = 0 #1024
   protocol                 = "tcp"
@@ -37,6 +28,27 @@ resource "aws_security_group_rule" "cluster_outbound" {
   source_security_group_id = aws_security_group.eks_nodes.id
   to_port                  = 65535
   type                     = "egress"
+}
+
+# The next two are to fix problem of AWS ALB Webhook service connection refused
+resource "aws_security_group_rule" "cluster_alb_webhook_inbound" {
+  description       = "Open communication to AWS Load Balancer webhook service"
+  from_port         = 9443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.eks_cluster.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  to_port           = 9443
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "cluster_outbound_all" {
+  description       = "Allow cluster API Server to communicate everybody"
+  from_port         = 0 
+  protocol          = "-1"
+  security_group_id = aws_security_group.eks_cluster.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  to_port           = 0
+  type              = "egress"
 }
 
 
